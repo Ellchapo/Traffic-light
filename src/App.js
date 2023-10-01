2a6178bf8a6671bc7b2a9d5fcd43d2dd24df9fba
AAKASH SINGH <72805470+Ellchapo@users.noreply.github.com>
2023-10-01 21:01:11 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState ,useEffect} from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [index, setIndex] = useState();
+  const [bl, setb] = useState(0);
+  const lightDurations = [3000, 2000, 1000];
+  
+  useEffect(()=>{
+   setTimeout(()=>{
+     setb((p)=>(p+1)%3)
+   },lightDurations[bl])
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="traffic-light" >
+    <div className="light"
+    style={{backgroundColor:'red' ,opacity:bl===0?1:0.4}}
+    ></div>
+     <div className="light"
+    style={{backgroundColor:'yellow' ,opacity:bl===1?1:0.2}}
+    ></div>
+     <div className="light"
+    style={{backgroundColor:'green' ,opacity:bl===2?4:0.2}}
+    ></div>
     </div>
+  
   );
 }
-
-export default App;
